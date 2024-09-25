f601d6db3f311942102ebc459cfefcca2c7f3bcb
Pedro Guilherme <pedropgfo@gmail.com>
2024-09-25 14:51:32 -0300

    Correcao email python

--- send_email.py
+++ send_email.py
@@ -3,20 +3,21 @@ import os
 from email.mime.text import MIMEText
 
 # Getting email recipient from environment variable
+email_username = os.getenv('EMAIL_USERNAME')
+email_password = os.getenv('EMAIL_PASSWORD')
 recipient = os.getenv('EMAIL_TO_NOTIFY')
-
 if recipient:
     # Define email content
     msg = MIMEText('Pipeline executed successfully!')
     msg['Subject'] = 'CI/CD Pipeline Notification'
-    msg['From'] = 'your-email@gmail.com'  # Insira seu email de envio aqui
+    msg['From'] = email_username  # Insira seu email de envio aqui
     msg['To'] = recipient
 
     # Gmail SMTP setup
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login('your-email@gmail.com', 'your-app-password')  # Use uma senha de app
-        server.sendmail('your-email@gmail.com', recipient, msg.as_string())
+        server.login(email_username, email_password)  # Use uma senha de app
+        server.sendmail(email_username, recipient, msg.as_string())
         server.quit()
         print("Email sent successfully to", recipient)
     except Exception as e:
