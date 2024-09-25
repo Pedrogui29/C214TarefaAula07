eb07530d9dfc4e6cc5ae2be6e51547b7ac8ed54d
Pedro Guilherme <pedropgfo@gmail.com>
2024-09-25 14:20:05 -0300

    Correcao email Pipe line

--- send_email.sh
+++ send_email.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Variáveis de ambiente
-EMAIL="$1"
-SUBJECT="Pipeline Notification"
-BODY="Pipeline executado com sucesso!"
+# O e-mail será passado como o primeiro argumento
+EMAIL_TO_NOTIFY=$1
+
+# Corpo do e-mail
+SUBJECT="CI/CD Pipeline Notification"
+MESSAGE="Pipeline executado com sucesso!"
+SENDER="ci-cd@example.com"
+
+# Montando o e-mail
+echo "Subject: $SUBJECT" > /tmp/email.txt
+echo "$MESSAGE" >> /tmp/email.txt
 
 # Enviando o e-mail
-echo -e "Subject: $SUBJECT\n\n$BODY" | sendmail -v $EMAIL
+sendmail "$EMAIL_TO_NOTIFY" < /tmp/email.txt
